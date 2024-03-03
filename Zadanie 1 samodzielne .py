52e95d768a52c6cc28c66d3ff88a5d5f63fc40a7
Artur <109029105+Arturitopopito@users.noreply.github.com>
2024-03-03 13:08:30 +0100

    Add files via upload

---
+++ Zadanie 1 samodzielne .py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import math as math
+
+
+# In[2]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+days = [1,2,3,4,5]
+
+
+# In[4]:
+
+
+amount = [1,5,20,450,700]
+
+
+# In[5]:
+
+
+plt.plot(days, amount)
+
+
+# In[ ]:
+
+
+p
+
